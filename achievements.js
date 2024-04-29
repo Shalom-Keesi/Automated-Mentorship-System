c887b40d2b5c7e017fe4910d3a2218545d34f52a
Shalom-Keesi <shalomkeesi@gmail.com>
2024-04-29 12:45:59 +0300

    added more changes

--- achievements.js
+++ achievements.js
@@ -1,20 +1,21 @@
-function addAchievement() {
-    const input = document.getElementById("achievements");
-    const taskName = input.value;
-    if (taskName === "") {
-    alert("Please enter an achievement.");
-    return;
-    }
-    input.value = "";
-    const list = document.getElementById("todo-list");
-    const li = document.createElement("li");
-    li.textContent = taskName;
+// function addAchievement() {
+//     const input = document.getElementById("achievements");
+//     const taskName = input.value;
+//     if (taskName === "") {
+//     alert("Please enter an achievement.");
+//     return;
+//     }
+//     input.value = "";
+//     const list = document.getElementById("todo-list");
+//     const li = document.createElement("li");
+//     li.textContent = taskName;
     
-    var deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.onclick = function () {
-    list.removeChild(li);
-    };
-    li.appendChild(deleteButton);
-    list.appendChild(li);
-    }
+//     var deleteButton = document.createElement("button");
+//     deleteButton.textContent = "Delete";
+//     deleteButton.onclick = function () {
+//     list.removeChild(li);
+//     };
+//     li.appendChild(deleteButton);
+//     list.appendChild(li);
+//     }
+
